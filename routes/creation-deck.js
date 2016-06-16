5ff59db8db9d7d3d8b4bfa706fe70598168d49a9
Jouin Alexis <alexis.jouin@live.fr>
2016-06-16 11:06:29 +0200

    Correction Gestion des Deck
    FIX  : amélioration de l'aglo qui récupère les carte en json dans la
    page des gestion de deck

--- routes/creation-deck.js
+++ routes/creation-deck.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var core = require('../core');
 var fs = require('fs');
+var path = require('path');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -9,55 +10,37 @@ router.get('/', function(req, res, next) {
     res.redirect('/connexion');
   }else{
 
-    var pokemonsXY = [];
-    var pokemonsGenerations = [];
-    var pokemonsPoingsFurieux = [];
-    var energy = [];
+      var tabPokemon=[];
+      var filesGeneration = fs.readdirSync('../data/XY/Generations/');
+      var filesPoingsFurieux = fs.readdirSync('../data/XY/Poings_Furieux/');
+      var filesXY = fs.readdirSync('../data/XY/XY/');
 
-    for (var i = 1; i < 49; i++){
-      var file = '../data/XY/XY/' + i + '.json';
-      var pokemon_cardXY = new core.Builder().createFromJSON(fs.readFileSync(file, 'utf8'));
-      var pokemonXY = new core.Pokemon(pokemon_cardXY);
-      pokemonsXY.push(pokemonXY.to_object());
+      filesGeneration.forEach(function (file) {
+          if (path.extname(file) === '.json') {
+              var card = new core.Builder().createFromJSON(fs.readFileSync('../data/XY/Generations/' + file, 'utf8'));
+              tabPokemon.push(card.to_object());
+          }
+      });
 
-    }
-
-    for (var i = 1; i < 58; i++) {
-
-       try {
-        var file = '../data/XY/Generations/' + i + '.json';
-        fs.accessSync(file, fs.F_OK);
-
-        var pokemon_cardGenerations = new core.Builder().createFromJSON(fs.readFileSync(file, 'utf8'));
-        var pokemonGenerations = new core.Pokemon(pokemon_cardGenerations);
-        pokemonsGenerations.push(pokemonGenerations.to_object());
-
-       }
-       catch(e) {
-        console.log("test");
-       }
-
-    }
-
-    for (var i = 1; i < 59; i++) {
-
-      try {
-        var file = '../data/XY/Poings_Furieux/' + i + '.json';
-        fs.accessSync(file, fs.F_OK);
-
-        var pokemon_cardPoingsFurieux = new core.Builder().createFromJSON(fs.readFileSync(file, 'utf8'));
-        var pokemonPoingsFurieux = new core.Pokemon(pokemon_cardPoingsFurieux);
-        pokemonsPoingsFurieux.push(pokemonPoingsFurieux.to_object());
-
-      }
-      catch(e) {
-        console.log("test");
-      }
-
-    }
+      filesPoingsFurieux.forEach(function (file) {
+          if (path.extname(file) === '.json') {
+              var card = new core.Builder().createFromJSON(fs.readFileSync('../data/XY/Poings_Furieux/' + file, 'utf8'));
+              tabPokemon.push(card.to_object());
+          }
+      });
 
 
-    res.render('creation-deck', { card: pokemonsXY, card2: pokemonsGenerations, card3: pokemonsPoingsFurieux  });
+      filesXY.forEach(function (file) {
+          if (path.extname(file) === '.json') {
+              var card = new core.Builder().createFromJSON(fs.readFileSync('../data/XY/XY/' + file, 'utf8'));
+              tabPokemon.push(card.to_object());
+          }
+      });
+
+
+
+
+    res.render('creation-deck', { cards: tabPokemon  });
 
   }
 });
